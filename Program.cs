fc49b246c25f31fc272cf1aa8f47022fbf21450c
Enion Ismaili <Enion.Ismaili@bgt.school>
2024-04-25 21:09:34 +0200

    cities in dictionary are placed to arrays

--- Program.cs
+++ Program.cs
@@ -1,59 +1,57 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Car_plates
 {
     internal class Program
     {
-        //here is declared a method that generates the formatted plate number
-        static string GeneratePlateNumber(string shifrat)
+        //here is declared a method to generate the formatted plate number
+        static string GeneratePlateNumber(string code)
         {
-            Random random = new Random(); //it creates a new random object
-            int numrat = random.Next(100, 1000);//it generates a number from 100 to 999
-            char shkronjaPare = (char)('A' + random.Next(0, 26)); //this generates a letter from A to Z, 0 is "A", 26 is "Z"
-            char shkronjaDyte = (char)('A' + random.Next(0, 26)); // this generates a letter too, in the same way as the first one, for the second letter of the plates 
-            
-            
-            return $"{shifrat} / {numrat} - {shkronjaPare}{shkronjaDyte}";//it returns the format of plate number
+            Random random = new Random(); //here is created a new random object
+            int numbers = random.Next(100, 1000); //this generates a number from 100 to 1000
+            char firstLetter = (char)('A' + random.Next(0, 26)); //this generates a letter from "A" to "Z"
+            char secondLetter = (char)('A' + random.Next(0, 26)); //this does the same thing as above line, generates a letter from "A" to "Z"
+
+            return $"{code} / {numbers} - {firstLetter}{secondLetter}"; //this returns the formatted plate number
         }
 
         static void Main(string[] args)
-        { 
-            A:
-            Console.WriteLine("Shenoje qytetin: "); //it asks the user to enter the name of the city
-            string komuna = Console.ReadLine().ToLower(); //this reads, stores and converts to lowercase the whole string entered by the user
-         
-            //here is created a dictionary that will map each string with its property that is also string in this case
-            var shifrat = new Dictionary<string, string>
+        {
+            //here is created a dictionary to map city codes to city names, using arrays to map multiple cities in a city code
+            var codesToCities = new Dictionary<string, string[]>
             {
-                {"prishtine", "01" },
-                {"podujeve", "01" },
-                {"mitrovice", "02" },
-                {"skenderaj", "02" },
-                {"vushtrri", "02" },
-                {"peje", "03" },
-                {"kline", "03" },
-                {"prizren", "04" },
-                {"suhareke", "04" },
-                {"ferizaj", "05" },
-                {"shtime", "05" },
-                {"gjilan", "06" },
-                {"gracanice", "06" },
-                {"gjakove", "07" },
-                {"malisheve", "07" }
+                {"01", new string[] {"prishtine", "podujeve", "lipjan", "drenas"}},
+                {"02", new string[] {"mitrovice", "skenderaj", "vushtrri"}},
+                {"03", new string[] {"peje", "kline", "istog", "decan"}},
+                {"04", new string[] {"prizren", "suhareke"}},
+                {"05", new string[] {"ferizaj", "shtime"}},
+                {"06", new string[] {"gjilan", "gracanice"}},
+                {"07", new string[] {"gjakove", "malisheve"}}
             };
 
-            if (shifrat.ContainsKey(komuna)) //this checks if the entered city by the user is in the dictionary
-            {
-                string tabelat = GeneratePlateNumber(shifrat[komuna]); //this calls the method above this main program, and generates the formatted plate number
-                Console.WriteLine("Targat tuaja: " + tabelat); //this prints the formatted plate number
-            }
-            else //if the condition above isn't met, then it will print the message below
-            {
-                Console.WriteLine("Emri i qytetit nuk eshte dhene sic duhet!");
-                goto A;
-            }
+            // Invert the dictionary to map from city names to codes
+            var citiesToCodes = codesToCities
+                .SelectMany(pair => pair.Value, (pair, city) => new { City = city, Code = pair.Key })
+                .ToDictionary(pair => pair.City, pair => pair.Code);
 
+            while (true)
+            {
+                Console.WriteLine("Shënoni qytetin tuaj: "); //it prompts the user to enter the city name
+                string city = Console.ReadLine().ToLower(); //it reads the input and converts it to lowercase
 
+                if (citiesToCodes.TryGetValue(city, out string code))
+                {
+                    string plate = GeneratePlateNumber(code); //this generates the formatted plate number
+                    Console.WriteLine("Targat tuaja: " + plate); //it displays the plate number on console
+                    break;
+                }
+                else //if the condition above isn't met, then it prints the message below
+                {
+                    Console.WriteLine("Emri i qytetit nuk është dhënë siç duhet! Provoni përsëri.");
+                }
+            }
         }
     }
 }
